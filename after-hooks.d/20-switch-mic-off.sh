4057e84a23ec562c06a9dd3432903d8e0929a410
Hossam Hammady <hossam@rayyan.ai>
2021-11-01 19:30:21 -0400

    Implement switch mic on/off using http

--- after-hooks.d/20-switch-mic-off.sh
+++ after-hooks.d/20-switch-mic-off.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 echo "Switching mic off"
+curl -f http://192.168.1.73:8888/press/bank/1/27
--- before-hooks.d/20-switch-mic-on.sh
+++ before-hooks.d/20-switch-mic-on.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 echo "Switching mic on"
+curl -f http://192.168.1.73:8888/press/bank/1/26
