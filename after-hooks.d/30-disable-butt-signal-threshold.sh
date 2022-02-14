91489b6bdda40b5122acd4003d18b605c795db7a
Hossam Hammady <github@hammady.net>
2022-02-13 20:34:47 -0500

    Disconnect any streams in the after hooks

--- after-hooks.d/30-disable-butt-signal-threshold.sh
+++ after-hooks.d/30-disable-butt-signal-threshold.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 echo "Setting streaming signal threshold on butt to 0 (disable)"
 /usr/local/bin/butt-client -a 192.168.1.73 -M 0
+sleep 1
+echo "Disconnecting any streams"
+/usr/local/bin/butt-client -a 192.168.1.73 -d
 
